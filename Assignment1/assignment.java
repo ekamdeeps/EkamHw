1ce055463c979c8da87d5c1a0c8d1838a269a714
ekamdeeps <ekamdeeps60@gmail.com>
2023-11-29 19:49:34 -0800

    Added first assignment

---
+++ Assignment1/assignment.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class assignment {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+                System.out.println("Enter user age");
+        int age=sc.nextInt();
+       if(age>=18)
+           System.out.println("Eligible to vote");
+       else if(age<=18 && age>0)
+           System.out.println("Ineligible to vote");
+       else
+           System.out.println("Invalid age entered");
+
+
+
+    }
+}
